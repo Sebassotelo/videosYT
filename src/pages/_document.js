3d8b31787f2574ba46be82313d2571d6b03b52bf
Sebas <sebassotelo97@gmail.com>
2023-09-13 22:00:45 -0300

    cambio de correo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+
 export default function Document() {
   return (
     <Html lang="es">
       <Head />
+
       <body>
         <Main />
         <NextScript />
